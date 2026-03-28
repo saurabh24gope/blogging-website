72259a6912a97716bf5cce02c26b3ea237eca0cc
saurabh24gope <saurabhgope@gmail.com>
2026-03-28 09:42:14 +0530

    modified latest app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db' = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
